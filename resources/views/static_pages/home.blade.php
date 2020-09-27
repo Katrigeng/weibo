ff23650d95dbf18b1c52d3cd5a4f9ef785455114
Katrigeng <453875798@qq.com>
2020-09-27 15:15:10 +0000

    initial style

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,11 @@
 @extends('layouts/default')
 @section('content')
-    <h1>主页你你你</h1>
+  <div class="home">
+    <h1>Hello Laravel</h1>
+    <p class="ziti">
+      你现在所看到的是<a href="https://learnku.com/courses/laravel-essential-training">Laravel 入门教程</a>的示例项目主页。
+    </p>
+    <p>一切从这里开始</p>
+    <a class="btn btn-success btn-lg" href="/" role="button">现在注册</a>
+  </div>
 @endsection
