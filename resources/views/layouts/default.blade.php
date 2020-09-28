68be97c9ae8de9427ae9ea621792856151183018
Katrigeng <453875798@qq.com>
2020-09-28 03:28:33 +0000

    static huancun

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,12 +5,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>@yield('title','WeiBo')</title>
-  <link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container_1">
+    <div class="container">
       <a class="navbar-brand" href="/">Weibo App</a>
       <ul class="navbar-nav">
         <li class="nav-item"><a class="nav-link" href="/">帮助</a></li>
